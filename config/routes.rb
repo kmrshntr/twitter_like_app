cc1f10d5e962359ef7e460d4d575aeaa8235217c
Sodai Hirai <hiraisoudai@gmail.com>
2018-09-27 15:00:39 +0900

    Finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   #static_pages
   root 'static_pages#home'
-  get  '/help',    to:  'static_pages#help'
-  get  '/about',   to:  'static_pages#about'
-  get  '/contact', to: 	'static_pages#contact'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  #users
+  get  '/signup',  to: 'users#new'
 end
