7a687f516249f2ab1f268cdb4524614b6f9d46ba
Sodai Hirai <hiraisoudai@gmail.com>
2018-09-28 12:17:29 +0900

    Finish basic-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   #static_pages
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
   #users
-  get  '/signup',  to: 'users#new'
+  get    '/signup',  to: 'users#new'
   resources :users
+  #sessions
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
