85963cf74a87e5ef0dfd8829872812ab6b8e72c8
Sodai Hirai <hiraisoudai@gmail.com>
2018-09-30 14:54:17 +0900

    Finish password-reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   #static_pages
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
@@ -13,4 +15,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   #accountactivations
   resources :accout_activations, only: [:edit]
+  #password_resets
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
---
+++ db/migrate/20180930012739_rename_request_column_to_users.rb
@@ -0,0 +1,5 @@
+class RenameRequestColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+  	rename_column :users, :reset_set_at, :reset_sent_at
+  end
+end
